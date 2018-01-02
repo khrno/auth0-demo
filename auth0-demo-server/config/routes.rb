6f466d90665a1179bc8c903a453fb06f818079d1
Pablo Ortega Mesa <portega@modyo.com>
2018-01-02 17:48:38 -0300

    Adding user model and user controller

--- auth0-demo-server/config/routes.rb
+++ auth0-demo-server/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
   root 'welcome#index'
   get 'welcome/index'
 end
--- auth0-demo-server/db/schema.rb
+++ auth0-demo-server/db/schema.rb
@@ -11,6 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180102204627) do
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username",   limit: 255
+    t.string   "email",      limit: 255
+    t.string   "password",   limit: 255
+    t.string   "token",      limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
 end
