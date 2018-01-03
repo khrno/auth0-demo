237a03383ec149e813a542ce3a3286abaca17139
Pablo Ortega Mesa <portega@modyo.com>
2018-01-03 10:32:04 -0300

    Adding users table

--- auth0-demo-server/app/controllers/users_controller.rb
+++ auth0-demo-server/app/controllers/users_controller.rb
@@ -1,4 +1,17 @@
 class UsersController < ApplicationController
+  before_action :load_users, only: :index
+
+  def initialize
+    super
+    @active = 'users'
+  end
+
   def index
+  end
+
+  private
+
+  def load_users
+    @users = User.all
   end
 end
